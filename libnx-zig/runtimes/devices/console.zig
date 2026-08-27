73a3527165f00519b763a22108e9c9b07990695a
José Henrique <146463048+henrique-electric@users.noreply.github.com>
2026-08-27 13:05:16 -0300

    add calling conventions to function pointers

--- libnx-zig/runtimes/devices/console.zig
+++ libnx-zig/runtimes/devices/console.zig
@@ -2,11 +2,11 @@ pub const PrintConsole = opaque {};
 
 /// Renderer interface for the console.
 pub const ConsoleRenderer = extern struct {
-    init:           *fn(con: *PrintConsole) bool,
-    deinit:         *fn(con: *PrintConsole) void,
-    drawChar:       *fn(con: *PrintConsole, x: i32, y: i32, c: i32) void,
-    scrollWindow:   *fn(con: *PrintConsole) void,
-    flushAndSwap:   *fn(con: *PrintConsole) void
+    init:           *const fn(con: *PrintConsole) callconv(.c) bool,
+    deinit:         *const fn(con: *PrintConsole) callconv(.c) void,
+    drawChar:       *const fn(con: *PrintConsole, x: i32, y: i32, c: i32) callconv(.c) void,
+    scrollWindow:   *const fn(con: *PrintConsole) callconv(.c) void,
+    flushAndSwap:   *const fn(con: *PrintConsole) callconv(.c) void
 };
 
 /// A font struct for the console.
